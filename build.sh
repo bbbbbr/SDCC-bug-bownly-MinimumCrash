e01a69eb0f03b4614c4f79b5c8934289e6640f5b
bbbbbr <bbbbbr@users.noreply.github.com>
2023-06-24 12:27:08 -0700

    add linux version of build script

---
+++ build.sh
@@ -0,0 +1,21 @@
+rm o/*.o
+rm o/*.lst
+rm o/*.asm
+rm o/*.sym
+
+mkdir -p o
+
+~/gbdev/gbdk/gbdk_4.2.0/gbdk/bin/lcc -debug -Wa-l -Wf-bo2 -c -o o/fontTiles.o Assets/Tiles/fontTiles.c
+~/gbdev/gbdk/gbdk_4.2.0/gbdk/bin/lcc -debug -Wa-l -Wf-bo2 -c -o o/TadTiles.o Assets/Tiles/TadTiles.c
+
+~/gbdev/gbdk/gbdk_4.2.0/gbdk/bin/lcc -debug -Wa-l -Wf-bo2 -c -o o/LevelState.o States/LevelState.c
+
+~/gbdev/gbdk/gbdk_4.2.0/gbdk/bin/lcc -debug -Wa-l -c -o o/common.o Engine/common.c
+
+~/gbdev/gbdk/gbdk_4.2.0/gbdk/bin/lcc -debug -Wa-l -c -o o/main.o main.c
+~/gbdev/gbdk/gbdk_4.2.0/gbdk/bin/lcc -debug -Wl-yt3 -Wl-yoA -Wl-ya4 -o MinimumCrash.gb o/*.o
+
+
+# These both work around the issue
+# -Wf--nolospre 
+# -Wf--max-allocs-per-node10000 
